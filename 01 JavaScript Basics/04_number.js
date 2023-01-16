73970e4af9d64737d7ab83e6a13671a54b28412b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-16 15:17:51 +0600

    number basic added

---
+++ 01 JavaScript Basics/04_number.js
@@ -0,0 +1,48 @@
+let radius=10;
+const pi=3.1416;
+
+//console.log(radius,pi);
+
+//Math operators {add(+),multiplication(*),subtraction(-),division(/),power(**), reminder(%)} --------------------------->
+
+//let result=radius*pi;
+
+//let result=radius % 3; 
+
+//let result=pi*radius**2;
+
+//oder of operation ( B I(indices) D M A S )------------------------------------------>
+
+//let result=pi+(radius*1)**2;
+
+//console.log(result);
+
+// (Increment and decrement)------------------------------------------>
+
+//radius=radius+1;
+
+//radius++ ;
+
+//radius-- ;
+
+//radius +=10 ;
+
+//radius -=5;
+
+//radius *=2;
+
+//radius /=2;
+
+//console.log(radius);
+
+//NAN-(not a number)----------------------------------------------------------->
+
+//console.log(5/'hello');
+
+//console.log(5*'hello');
+
+//concat number with string ----------------------------------------------------->
+
+let result='hello'+ ' ' +radius;
+
+console.log(result);
