73ab7fbcea51c3b176102a474126c4a3dc441b02
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-17 16:55:11 +0600

    type conversion added

---
+++ 01 JavaScript Basics/10_Type conversion.js
@@ -0,0 +1,21 @@
+//type of conversion------------->
+ 
+//let score='100';
+//console.log(score+1);
+
+//lets convert it first to number->
+
+//let score='100';
+//score=Number(100);
+//console.log(score+1);
+//console.log(typeof score)
+
+//let result=String(20);
+
+//let result=Boolean(100);
+//let result=Boolean(-10)
+//let result=Boolean(0);
+//let result=Boolean('0');
+let result=Boolean('');
+
+console.log(result,typeof result);
