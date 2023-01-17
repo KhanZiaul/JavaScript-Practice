8f6a5408d790c16533cc1ee373284eec14847c51
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-17 15:07:59 +0600

    booleans and comparisons added

---
+++ 01 JavaScript Basics/08_booleans and comparisons.js
@@ -0,0 +1,36 @@
+//Methods can returns booleans----------------------->
+
+//let email='khanzia16phy102@gmail.com';
+
+//let result=email.includes('16');
+//let result=email.includes('k');
+
+//console.log(result);
+
+//let names=['zia','khan','mega'];
+
+//let result= names.includes('mega');
+//let result= names.includes('moni');
+
+//console.log(result);
+
+//comparisons operators--------------------->
+
+//let age=35;
+
+//console.log(age == 25);
+//console.log(age == 35);
+//console.log(age>30);
+//console.log(age <= 35);
+//console.log(age != 36);
+//console.log(age != 35);
+
+let names='mega';
+
+console.log(names == 'moni');
+console.log(names == 'mega');
+console.log(names == 'Mega');
+console.log(names >= 'mega');
+console.log(names < 'zia');
+console.log(names != 'mega');
+console.log(names < 'mega');
