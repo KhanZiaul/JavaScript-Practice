5bd8cfc79cfb10096f4fa89c2daadb02dcc03797
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-17 15:57:41 +0600

    loose and strict comparisons added

---
+++ 01 JavaScript Basics/09_Loose vs strict comparisons.js
@@ -0,0 +1,19 @@
+//Loose comparisons(different types data can be equal)
+
+//let age=20;
+//console.log(age == 20);
+//console.log(age != 23);
+
+//let age='20';
+//console.log(age == 20);
+//console.log(age != 23);
+
+//Strict comparisons(different types can not be data can be equal)
+
+//let age=20;
+//console.log(age === 20);
+//console.log(age !== 23);
+
+let age='20';
+console.log(age === 20);
+console.log(age !== 23);
