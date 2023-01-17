3c5d20c0661b7f1519515e38d2e5cbc028437e6a
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-17 14:20:59 +0600

    null and undefiend added

---
+++ 01 JavaScript Basics/07_Null and Undefiend.js
@@ -0,0 +1,12 @@
+
+// undefined ------------------------------->
+
+//let age;
+
+//console.log(age,age+3,`the age is ${age}`);
+
+//Null---------------------->
+
+let age=null;
+
+console.log(age,age+3,`the age is ${age}`);
