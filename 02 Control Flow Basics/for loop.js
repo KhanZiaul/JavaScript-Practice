8715de5cbaba35d9cdc90aa5a985c95493b100a8
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 15:47:31 +0600

    for loop added

---
+++ 02 Control Flow Basics/for loop.js
@@ -0,0 +1,21 @@
+//for loop
+
+for (let i=0; i<5; i++){
+
+    console.log(i);
+}
+
+let names = ['khan', 'zia', 'mega'];
+
+// for (let i =0; i < names.length; i++){
+
+//  console.log(i, names[i]);
+// }
+
+
+for (let i =0; i < names.length; i++){
+
+    let html = `Client Name ${names[i]}` ;
+
+    console.log(html);
+}
