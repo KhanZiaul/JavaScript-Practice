e8c7f518b28e700b17b2790d0ded0c1e9207db48
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 16:21:25 +0600

    do while loop done and fix some problem

---
+++ 02 Control Flow Basics/do while loop.js
@@ -0,0 +1,32 @@
+//do while loop
+
+//--------------------------------------->
+
+// let i=5;
+
+// do{
+
+// console.log(i); 
+
+// i++;
+
+// }
+
+// while(i<5);
+
+//------------------------------------->
+
+let names = ['khan', 'zia', 'mega'];
+
+let i = 1;
+
+do{
+
+console.log(i, names[i]);
+
+i++; 
+
+}
+
+while (i < names.length);
+
