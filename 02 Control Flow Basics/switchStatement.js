2235f01c66d0d4fb0fa4bd85d7f93f05cf1374e7
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 18:18:25 +0600

    switch statement done

---
+++ 02 Control Flow Basics/switchStatement.js
@@ -0,0 +1,25 @@
+//switch statement
+
+let grade = 'A';
+
+switch(grade){
+
+    case 'A':
+        console.log('You Got A');
+        break;
+    case 'B':
+        console.log('You Got B');
+        break;
+    case 'C':
+        console.log('You Got C');
+        break;
+    case 'D':
+        console.log('You Got D');
+        break;
+    case 'E':
+        console.log('You Got E');
+        break;
+    default:
+        console.log('You Got Big F . Which Means You Looser');
+
+}
