6bee04cd6dad2a9116119d80fccfd74e365a25ff
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 17:48:41 +0600

    updated

--- 02 Control Flow Basics/break and continue.js
+++ 02 Control Flow Basics/break and continue.js
@@ -4,13 +4,13 @@ let scores = [30,40,70,90,0,0,100,110];
 
 for(let i=0; i<scores.length; i++){
 
-if(scores[i]==0){
+if(scores[i] === 0){
     continue;
 }
 
-console.log(scores[i]);
+console.log(scores[i]); 
 
-if(scores[i]==100){
+if(scores[i] === 100){
 
     console.log('congratulations!!  You done it');
     break;
