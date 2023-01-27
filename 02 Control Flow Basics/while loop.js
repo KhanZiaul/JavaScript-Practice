af44adddffc601d39f38101174130b1887bf8288
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 16:05:36 +0600

    while loop added

---
+++ 02 Control Flow Basics/while loop.js
@@ -0,0 +1,28 @@
+//while loop
+
+let i=0;
+
+//while (i<5){
+
+//  console.log(i);
+// i++;
+// }
+
+let names = ['khan', 'zia', 'mega'];
+
+// while (let i < names.length){
+
+//  console.log(i, names[i]);
+
+// i++; 
+
+// }
+
+while (i < names.length){
+
+    let html = `Client Name ${names[i]}` ;
+
+    console.log(html);
+
+    i++
+}
