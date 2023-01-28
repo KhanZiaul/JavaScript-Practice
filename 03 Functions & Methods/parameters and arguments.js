ac0712cf3fb04ec4e16ed999b69d39f3abaf8d24
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-28 16:45:33 +0600

    updated

---
+++ 03 Functions & Methods/parameters and arguments.js
@@ -0,0 +1,12 @@
+//parameters and arguments
+
+const speak= function(client='khan',time='morning'){
+
+console.log(`good ${time} ${client}`);
+};
+
+speak();
+
+speak('zia');
+
+speak('zia','evening');
