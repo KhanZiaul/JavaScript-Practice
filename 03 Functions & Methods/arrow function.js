352b42495a14c49414ccbb58cf87b17062ad7992
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-28 19:38:00 +0600

    arrow functions done

---
+++ 03 Functions & Methods/arrow function.js
@@ -0,0 +1,29 @@
+// arrow function
+
+// const area = (radius) =>{
+    
+//     return 3.1416 * radius**2;
+// };
+
+// const areaIs = area(5);
+// console.log(areaIs);
+
+//reduce it when you have one parameters no need parentheses------->
+
+// const area = radius =>{
+    
+//     return 3.1416 * radius**2;
+// };
+
+// const areaIs = area(5);
+// console.log(areaIs);
+
+
+//reduce it (again) when you have one parameters no need to write return type-------------->
+
+const area = radius =>3.1416 * radius**2;
+
+const areaIs = area(5);
+console.log(areaIs);
+
+
