cc642a445812d9c4539f7ae64a03bd2583f13ea3
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-29 15:51:46 +0600

    updated

---
+++ 04 Object Literals/create an object.js
@@ -0,0 +1,18 @@
+// create an object
+
+const khanZia = {
+
+    firstName:'khan',
+    lastName: 'zia',
+    id : '16 PHY 102',
+};
+
+console.log(khanZia.lastName);
+
+console.log(khanZia['firstName']);
+
+khanZia.lastName = 'mega';
+
+console.log(khanZia);
+
+console.log(typeof khanZia);
