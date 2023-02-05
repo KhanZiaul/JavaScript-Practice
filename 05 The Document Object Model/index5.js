604ff7c9626dc286fd51b1e80db5e676f44d9e76
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-06 00:40:32 +0600

    index5.js done

---
+++ 05 The Document Object Model/index5.js
@@ -0,0 +1,9 @@
+// style property in javaScript
+
+const styles = document.querySelector('h2');
+
+styles.style.margin ='100px';
+
+styles.style.color ='purple';
+
+styles.style.margin =''; // remove any style just blank it again
