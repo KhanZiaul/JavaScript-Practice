935a016d47d7af4073956595a1b4e4d5f0c03fb3
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-05 19:56:47 +0600

    index2.js done

---
+++ 05 The Document Object Model/index2.js
@@ -0,0 +1,21 @@
+// get element by their id
+
+// const idIs = document.getElementById('idOne');
+
+// console.log(idIs); 
+
+// get element by their classes
+
+// const classesAre = document.getElementsByClassName('class');
+
+// console.log(classesAre); 
+
+// console.log(classesAre[0]);
+
+// get element by their tag names
+
+const tagsAre = document.getElementsByTagName('p');
+
+console.log(tagsAre);
+
+console.log(tagsAre[0]);
