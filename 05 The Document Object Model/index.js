90cce62d2731c4cf79f05ec806ae46a999429eb0
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-05 16:10:11 +0600

    index.js done

---
+++ 05 The Document Object Model/index.js
@@ -0,0 +1,25 @@
+// (querySelector) ------------------------------------------------>
+
+// const para = document.querySelector('p');
+
+// const para = document.querySelector('div.class');
+
+// const para = document.querySelector('body > div > h2');
+
+// console.log(para);
+
+// (querySelectorAll) ------------------------------------------------>
+
+// const paras = document.querySelectorAll('p');
+const paras = document.querySelectorAll('.class');
+
+//console.log(paras);
+
+//console.log(paras[0]);
+
+// paras.forEach(value => {
+
+//     console.log(value);
+// });
+
+console.log(paras);
