07275104b360461647ade1622fe501be51533d8b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-05 23:41:43 +0600

    index4.js done

---
+++ 05 The Document Object Model/index4.js
@@ -0,0 +1,21 @@
+// getting and setting attributes
+
+const attributes = document.querySelector('a');
+
+console.log(attributes.getAttribute('href'));
+
+attributes.setAttribute('href','https://www.programming-hero.com');
+
+console.log(attributes.getAttribute('href'));
+
+attributes.innerText = 'Programming-hero';
+
+const styles = document.querySelector('p');
+
+console.log(styles.getAttribute('class'));
+
+styles.setAttribute('class','success');
+
+console.log(styles.getAttribute('class'));
+
+styles.setAttribute('style','color:green');
