a2e58adabdc189c014695d60348ba65bee7a76b4
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-06 02:04:26 +0600

    index6.js done

---
+++ 05 The Document Object Model/index6.js
@@ -0,0 +1,28 @@
+// remove and add classes
+
+// const para = document.querySelector('p');
+
+// para.classList.add('error');
+
+// para.classList.remove('error');
+
+// para.classList.add('success');
+
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(para =>{
+
+    if(para.textContent.includes('error')){
+
+        para.classList.add('error');
+    }
+    if(para.innerText.includes('success')){
+
+        para.classList.add('success');
+    }
+});
+
+const title = document.querySelector('h2');
+
+title.classList.toggle('header');
