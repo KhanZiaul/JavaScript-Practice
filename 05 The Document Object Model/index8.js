9698595811f463bf7855f2255b022eb7590a6e98
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-06 17:04:12 +0600

    index8.js done

--- 05 The Document Object Model/index8.js
+++ 05 The Document Object Model/index8.js
@@ -6,3 +6,16 @@
 //     console.log('you clicked me');
 // });
 
+const iteams = document.querySelectorAll('li');
+
+iteams.forEach(function(item){
+    item.addEventListener('click',function(e){
+        // console.log('clicked item');
+
+       // console.log(e.target); // show sigle any iteam
+
+       // console.log(e.iteam); // show double any iteam
+
+       e.target.style.textDecoration = 'line-through';
+    });
+});
